2f04aa6a87c86e6ea8d7eda7fa8661d9334711f7
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-31 17:16:10 +0530

    Implemented AD LinkedList

---
+++ dsa_in_java/src/code/g/LinkedList.java
@@ -0,0 +1,93 @@
+package code.g;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    //addFirst
+    public void addFirst(int item) {
+        var n = new Node(item);
+        if (first == null)
+            first = last = n;
+        else {
+            n.next = first;
+            first = n;
+        }
+
+    }
+
+    //addLast
+    public void addLast(int item) {
+        var node = new Node(item);
+        if (first == null)
+            last = first = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    //deleteFirst
+    public void deleteFirst() {
+        //[1  2->3->4]
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (first == last) {
+            return;
+        }
+        var second = first.next;
+        first.next = null; // remove link
+        first = second;
+
+    }
+
+
+    //deleteLast
+    public void deleteLast() {
+        var curr = first;
+        while (curr.next.next != null) {
+            curr = curr.next;
+        }
+        last = curr;
+        curr.next = null;
+    }
+
+    //contains
+    public boolean contains(int key) {
+        var curr = first;
+        while (curr != null) {
+            if (curr.value == key)
+                return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    //indexOf
+    public int indexOf(int item) {
+        var curr = first;
+        int index = 0;
+        while (curr != null) {
+            if (curr.value == item)
+                return index;
+            curr = curr.next;
+            index++;
+        }
+        return -1; // not found
+    }
+}
--- dsa_in_java/src/code/g/Main.java
+++ dsa_in_java/src/code/g/Main.java
@@ -3,32 +3,46 @@ package code.g;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Main {
     public static void main(String[] args) {
         //int [] nums = new int[size];
-        int [] numbers = {10, 20, 30};
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
+//        int [] numbers = {10, 20, 30};
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+//
+//        // Array class
+//        System.out.println("\nArray class:");
+//        Array nums = new Array(3);
+//        nums.insert(4);
+//        nums.insert(5);
+//        nums.insert(4);
+//        nums.insert(10);
+//        nums.insert(89);
+//        nums.removeAt(3);
+//        nums.print();
+//
+//        // ArrayList
+//        System.out.println("\nArray list=");
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        System.out.println(list);
 
-        // Array class
-        System.out.println("\nArray class:");
-        Array nums = new Array(3);
-        nums.insert(4);
-        nums.insert(5);
-        nums.insert(4);
-        nums.insert(10);
-        nums.insert(89);
-        nums.removeAt(3);
-        nums.print();
+        // LinkedList
+        LinkedList list = new LinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
 
-        // ArrayList
-        System.out.println("\nArray list=");
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+
+        list.deleteFirst();
+        list.deleteLast();
+
         System.out.println(list);
-
-
     }
 }
